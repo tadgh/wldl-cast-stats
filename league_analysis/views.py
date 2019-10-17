e2ec1e8cde5bf9af6fccc11a8e693bab56542de5
Gary graham <garygrantgraham@gmail.com>
2019-10-16 18:55:54 -0700

    Update to example template for collaborators

--- league_analysis/views.py
+++ league_analysis/views.py
@@ -10,9 +10,10 @@ def live_data(request):
         return "hi!"
 
 def head_to_head(request):
-    team_stats = TeamRollingStatistics.objects.get(team__name="The Mike Gleesons")
+    team_one_stats = TeamRollingStatistics.objects.get(team__name="The Mike Gleesons")
+    team_two_stats = TeamRollingStatistics.objects.get(team__name="Sink Catz")
     context = {
-        "team_one_statistics": team_stats
+        "team_one_statistics": team_one_stats,
+        "team_two_statistics": team_two_stats
     }
-    #TODO STYLE THIS
     return render(request, "leagueanalysis/headtohead.html",context)
